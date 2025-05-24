ee0e71450077c202a425690b51a77f7a954ec0a0
Jehu-RND <j.casimiro@qstrike.com>
2025-05-24 21:25:44 +0800

    add components config

--- jesa_ui/utils.py
+++ jesa_ui/utils.py
@@ -3,6 +3,8 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 
 COMPONENTS = {
-    'button': os.path.join(BASE_DIR, 'components', 'button.html')
+    'button': os.path.join(BASE_DIR, 'components', 'button.j2'),
+    'input': os.path.join(BASE_DIR, 'components', 'input.j2'),
+    'select': os.path.join(BASE_DIR, 'components', 'select.j2')
     # add more components here
 }
